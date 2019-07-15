53fbda6b18ea9d30553b22b2c81a3be660fb96ea
vovkula <dmitriy.vovkula@gmail.com>
2019-07-15 23:10:47 +0300

    add write to file

--- template_agg_arista.py
+++ template_agg_arista.py
@@ -8,8 +8,11 @@ serv = input('Enter connected host: ')
 
 print('\n' + '-' * 40)
 
-with open('arista_lag.txt', 'r') as src:
-	print((src.read()).format(vlan,eth,serv,100 + int(eth),100 + int(eth),serv + ':AGG',vlan,100 + int(eth)))
+with open('arista_lag.txt', 'r') as src, open('arista_cfg.txt', 'w') as dest:
+	dest.write((src.read()).format(vlan, eth, serv, 100 + int(eth), 100 + int(eth), serv + ':AGG', vlan, 100 + int(eth)))
+
+with open('arista_cfg.txt', 'r') as dest:
+	print(dest.read())
 
 print('-' * 40)
 
