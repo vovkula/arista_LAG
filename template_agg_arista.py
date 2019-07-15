9645ceefdb962adb0f104b0c23d536d0369821c0
vovkula <dmitriy.vovkula@gmail.com>
2019-07-15 22:53:03 +0300

    add description

--- template_agg_arista.py
+++ template_agg_arista.py
@@ -9,7 +9,7 @@ serv = input('Enter connected host: ')
 print('\n' + '-' * 40)
 
 with open('arista_lag.txt', 'r') as src:
-	print((src.read()).format(vlan,eth,serv,100 + int(eth),100 + int(eth),serv,vlan,100 + int(eth)))
+	print((src.read()).format(vlan,eth,serv,100 + int(eth),100 + int(eth),serv + ':AGG',vlan,100 + int(eth)))
 
 print('-' * 40)
 
