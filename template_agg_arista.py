d02a0176adceb5f28b7888691f55d29ea56bf9d3
vovkula <52749537+vovkula@users.noreply.github.com>
2019-07-15 22:43:35 +0300

    Add files via upload

---
+++ template_agg_arista.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print('-' * 40)
+
+vlan = input('Enter VLAN id: ')
+eth = input('Enter interface number: ')
+serv = input('Enter connected host: ')
+
+print('\n' + '-' * 40)
+
+with open('arista_lag.txt', 'r') as src:
+	print((src.read()).format(vlan,eth,serv,100 + int(eth),100 + int(eth),serv,vlan,100 + int(eth)))
+
+print('-' * 40)
+
